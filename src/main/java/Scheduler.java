30fa2f08b4544d021253f18ffb196ffb1b3325ea
Salaikumar <saravanan1115@gmail.com>
2016-03-21 22:29:40 +0530

    Decoupled Scheduler class from File reading. Let the client take care of it.

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -14,27 +14,11 @@ public class Scheduler {
         itemQueues = new ItemQueues();
     }
 
-    public void schedule(String fileLocation) throws IOException {
-       List<String> lineItems = Files.readAllLines(Paths.get(fileLocation));
-        for ( String item : lineItems){
-            String[] properties = item.split(",");
-            String itemId = properties[0];
-            int merchantId = Integer.parseInt(properties[1]);
-            int marketPlaceId = Integer.parseInt(properties[2]);
-            String data_type = properties[4];
-            Priority priority = Priority.NORMAL;
-            if (properties[3].equalsIgnoreCase("HIGHEST") )
-                priority = Priority.HIGHEST;
-            else if (properties[3].equalsIgnoreCase("HIGH"))
-                priority = Priority.HIGH;
-            else if (properties[3].equalsIgnoreCase("LOW"))
-                priority = Priority.LOW;
-            else if (properties[3].equalsIgnoreCase("NORMAL"))
-                priority = Priority.NORMAL;
-            else if (properties[3].equalsIgnoreCase("LOWEST"))
-                priority = Priority.LOWEST;
-            itemQueues.add(new LineItem(itemId,merchantId,marketPlaceId,priority,data_type));
-        }
+    /*
+     * Schedule one item at a time - Decoupling the file reading part from Scheduler
+     */
+    public void schedule(LineItem item){
+        itemQueues.add(item);
     }
 
     /*
--- src/test/java/SchedulerTest.java
+++ src/test/java/SchedulerTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -12,15 +14,34 @@ import static org.junit.Assert.assertThat;
 public class SchedulerTest {
 
     @Test
-    public void shouldTestSchedule(){
+    public void shouldTestSchedule() throws IOException {
+
         Scheduler sample = new Scheduler();
-        try {
-            sample.schedule("/home/salaikumar/WebApps/Items");
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        List<String> lineItems = Files.readAllLines(Paths.get("/home/salaikumar/WebApps/Items"));
+
+        for ( String item : lineItems){
+            String[] properties = item.split(",");
+            String itemId = properties[0];
+            int merchantId = Integer.parseInt(properties[1]);
+            int marketPlaceId = Integer.parseInt(properties[2]);
+            String data_type = properties[4];
+            Priority priority = Priority.NORMAL;
+            if (properties[3].equalsIgnoreCase("HIGHEST") )
+                priority = Priority.HIGHEST;
+            else if (properties[3].equalsIgnoreCase("HIGH"))
+                priority = Priority.HIGH;
+            else if (properties[3].equalsIgnoreCase("LOW"))
+                priority = Priority.LOW;
+            else if (properties[3].equalsIgnoreCase("NORMAL"))
+                priority = Priority.NORMAL;
+            else if (properties[3].equalsIgnoreCase("LOWEST"))
+                priority = Priority.LOWEST;
+
+        sample.schedule(new LineItem(itemId,merchantId,marketPlaceId,priority,data_type));
+
         }
 
-//        ItemQueues itemQueues = sample.getItemQueues();
         List<LineItem> toSendList = sample.getItems();
         assertThat(toSendList.contains(new LineItem("ITEM1",12345,4,Priority.LOW,"ITEM")),is(true));
     }
